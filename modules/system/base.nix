fb1988dcaa9a5f08a88c52ce2c0cf509cc93c162
Anoni Mouse <nixprivacy@pm.me>
2025-07-26 22:49:31 +0100

    Add python cache

--- modules/system/base.nix
+++ modules/system/base.nix
@@ -181,6 +181,7 @@
       "https://claude-code.cachix.org"
       "https://nix-community.cachix.org"
       "https://devenv.cachix.org"
+      "https://nixpkgs-python.cachix.org"
     ];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" # Default NixOS
@@ -188,6 +189,7 @@
       "nix-devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" # Nix Community
       "community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU="
     ];
   };
 
