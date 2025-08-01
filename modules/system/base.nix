d860b2749557645689f9e60e8e0abcd8771da72f
Anoni Mouse <nixprivacy@pm.me>
2025-08-01 21:43:32 +0100

    Simplify broken audio! ;-)

--- modules/system/base.nix
+++ modules/system/base.nix
@@ -50,14 +50,14 @@
   };
 
   # Enable sound with PipeWire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
+  #services.pulseaudio.enable = false;
+  #security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
+    #  alsa.enable = true;
+    #  alsa.support32Bit = true;
+    #  pulse.enable = true;
+    #  jack.enable = true;
   };
 
   # Enable printing.
