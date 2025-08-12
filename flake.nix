cf54edf39c0a5d5212faeddc2fddebd06a3a75cb
Anoni Mouse <nixprivacy@pm.me>
2025-08-12 22:34:25 +0100

    Reorder and tidy flake inputs

--- flake.nix
+++ flake.nix
@@ -2,33 +2,39 @@
   description = "Modular NixOS configuration";
 
   inputs = {
+    # nixos
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # home-manager
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    # other flakes
+    hyprland.url = "github:hyprwm/Hyprland";
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     nvf.url = "github:notashelf/nvf";
-    hyprland.url = "github:hyprwm/Hyprland";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
 
-  outputs = { self, nixpkgs, home-manager, nvf, ... }@inputs:
-  let
+  outputs = {
+    self,
+    nixpkgs,
+    home-manager,
+    ...
+  } @ inputs: let
     system = "x86_64-linux";
   in {
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       inherit system;
       modules = [
         ./hosts/desktop/default.nix
-        nvf.nixosModules.default
+        inputs.nixosModules.default
       ];
-      specialArgs = { inherit inputs; };
+      specialArgs = {inherit inputs;};
     };
 
-    homeConfigurations."anoni@desktop" =
-      home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${system};
-        extraSpecialArgs = { inherit inputs; };
-        modules = [ ./home/anoni.nix ];
-      };
+    homeConfigurations."anoni@desktop" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      extraSpecialArgs = {inherit inputs;};
+      modules = [./home/anoni.nix];
+    };
   };
 }
