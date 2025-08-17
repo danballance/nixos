00eacaecf346adb591b15f7b2f2ca7e2dc8cd409
Anoni Mouse <nixprivacy@pm.me>
2025-08-17 17:50:49 +0100

    Add a function to fish that dumps some useful info about installed apps & configurations

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -12,6 +12,34 @@
       find = "fd";
       ps = "procs";
     };
+		
+		functions = {
+
+      nixinfo = ''
+        function nixinfo --description "Provides basic info about the NixOS envrionment"
+
+          for dir in (string split : -- $XDG_CONFIG_DIRS)
+            if test -d "$dir"
+              set_color cyan; echo "=== $dir ==="; set_color normal
+              # use the binary directly to bypass any ls aliasing
+              command lsd --tree --depth 2 -a --group-dirs first --classify "$dir"
+              echo
+            else
+              set_color yellow; echo "Missing: $dir"; set_color normal
+            end
+          end
+
+					set_color cyan
+					echo "/etc/profiles/per-user/anoni/"
+					ls -lah /etc/profiles/per-user/anoni/
+					echo
+
+					set_color cyan
+					echo "~/.config/"
+					ls -lah ~/.config/
+        end
+      '';
+		};
   };
 }
 
